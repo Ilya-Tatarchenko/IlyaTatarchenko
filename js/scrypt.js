b33325c4b5dd161ebeccd594662496af25831522
Ilya-Tatarchenko <tatarchenkoii2003@gmail.com>
2021-03-17 23:21:14 +0300

    v1.2

--- js/scrypt.js
+++ js/scrypt.js
@@ -4,3 +4,10 @@ $(document).ready(function() {
         $('body').toggleClass('lock');
     });
 });
+
+$(document).ready(function () {
+    $('.header_link').click(function (event) {
+       $('.header_burger, .header_menu').toggleClass('active');
+       $('body').toggleClass('lock');
+    });
+ });
